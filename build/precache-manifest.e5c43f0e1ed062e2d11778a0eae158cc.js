6d41dd871edb7f07550b9588ba03f4f846668d84
karineB <karinebondarenko03@gmail.com>
2021-09-04 10:42:57 +0500

    Add files via upload

---
+++ build/precache-manifest.e5c43f0e1ed062e2d11778a0eae158cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d908031ed317eddaeab81423b7d9997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "756f3ecfa41858863658",
+    "url": "/static/css/main.a1d94c57.chunk.css"
+  },
+  {
+    "revision": "50356d26cc302160cdf8",
+    "url": "/static/js/2.37b644e6.chunk.js"
+  },
+  {
+    "revision": "756f3ecfa41858863658",
+    "url": "/static/js/main.0ca97682.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/static/media/up.551b5c6b.svg"
+  }
+]);
